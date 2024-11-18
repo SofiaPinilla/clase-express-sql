0dbe11261ecb5b14272078dbbf14a3a56ba1e945
SofiaPinilla <contact@sofiapinilla.com>
2024-11-18 12:35:22 +0100

    controllers implemented

--- routes/posts.js
+++ routes/posts.js
@@ -1,51 +1,15 @@
 const express = require("express");
+const PostController = require("../controllers/PostController");
 const router = express.Router();
-const db = require("../config/database")
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  let sql = `INSERT INTO posts (title, body) values
-            ('${req.body.title}', '${req.body.body}');`;
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send("Post added...");
-  });
-});
+router.post("/", PostController.create);
 
-router.get("/", (req, res) => {
-    let sql = `SELECT * FROM posts`;
-    db.query(sql, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      //   res.send(result);
-      res.send({
-        description: "Aqui tienes las publicaciones",
-        posts: result,
-      });
-    });
-  });
-  router.get("/id/:id", (req, res) => {
-    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
-    db.query(sql, (err, result) => {
-      if (err) throw err;
-      res.send(result);
-    });
-  });
-  
-  router.put('/id/:id',(req,res)=>{
-    let sql = `UPDATE posts SET title = '${req.body.title}' WHERE id = ${req.params.id}`;
-    db.query(sql, (err,result)=> {
-      if(err) throw err;
-      res.send('Post updated...')
-    })
-  })
+router.get("/", PostController.getAll);
 
-  router.delete("/id/:id",(req,res)=>{
-    let sql = `DELETE FROM posts WHERE id = ${req.params.id};`
-    db.query(sql,(err,result)=>{
-        if(err)throw err;
-        res.send("Post deleted")
-    })
-})
+router.get("/id/:id", PostController.getById);
+
+router.put("/id/:id", PostController.update);
+
+router.delete("/id/:id", PostController.delete);
+
 module.exports = router;
